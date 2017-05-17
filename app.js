084e3a888faefce6a18d7402e2d6fbee36f27103
nfjones87 <nfjones87@gmail.com>
2017-05-17 12:15:03 -0700

    functional without event listeners

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const appState = {
 	results:[],
 };
 
+
+// API
+
 var youtubeBaseUrl = 'https://www.googleapis.com/youtube/v3/search'
 
 function getDataFromApi(searchTerm, callback){
@@ -13,8 +16,44 @@ function getDataFromApi(searchTerm, callback){
 	$.getJSON(youtubeBaseUrl, query, callback);
 }
 
-const logData = (data) => {
-	console.log(data);
-};
+//modify state
 
-getDataFromApi('Overwatch', logData);
+let modState = (data) => {
+	data.items.forEach(object => {
+		let image = object.snippet.thumbnails.high.url;
+		let title = object.snippet.title;
+		let desc = object.snippet.description;
+	let video = {
+		imageSrc: image,
+		title: title,
+		description: desc,
+	};
+	appState.results.push(video);
+
+	});
+}
+
+// render function
+
+let render = (state) => {
+	let html = ``;
+	appState.results.forEach(object =>{
+		html += `
+			<div class="video">
+				<h2>Title:${object.title}</h2>
+				<img src="${object.imageSrc}">
+				<p>${object.description}</p>
+			</div>
+		`
+	});
+	
+	$('.resultsContainer').html(html);
+}
+
+
+
+
+
+//event listener
+
+getDataFromApi("overwatch", modState);
